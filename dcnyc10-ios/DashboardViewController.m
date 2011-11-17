77687f17d54fb385bf561efa4cfa71881ce6b6b1
Roger López <lopez.roger@gmail.com>
2011-11-16 19:39:46 -0500

    fix table cell backgrounds when stretched for landscape orientation

--- dcnyc10-ios/DashboardViewController.m
+++ dcnyc10-ios/DashboardViewController.m
@@ -71,8 +71,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 - (IBAction)gotoSessions:(id)sender
