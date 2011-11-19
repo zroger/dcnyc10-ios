4b8ea1f2ee1aa9609382c6bcc7d5f6e66cf68fe4
Roger López <lopez.roger@gmail.com>
2011-11-19 18:36:05 -0500

    added NotificationHelper view

---
+++ dcnyc10-ios/NotificationHelper.h
@@ -0,0 +1,13 @@
+//
+//  NotificationHelper.h
+//  dcnyc10-ios
+//
+//  Created by Roger Lopez on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationHelper : UITableViewController
+
+@end
